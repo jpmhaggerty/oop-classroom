ef31b6e0484ddb341e471f7245840555b5c2582e
James Haggerty <jpm.haggerty@gmail.com>
2021-09-16 17:19:24 -0400

    computer.js built out

--- src/computer.js
+++ src/computer.js
@@ -1,5 +1,13 @@
 class Computer {
-  
+  constructor(name, operatingSystem, processor, memory, graphics) {
+    this.name = name;
+    this.operatingSystem = operatingSystem;
+    this.processor = processor;
+    this.memory = memory;
+    this.graphics = graphics;
+    this.on = false;
+  }
+
 }
 
 module.exports = Computer
