a52e14aa538cc3c669d68d32fef557e06f4b0b11
James Haggerty <jpm.haggerty@gmail.com>
2021-09-17 11:24:07 -0400

    Complete computer, desk, and marker classes

--- src/marker.js
+++ src/marker.js
@@ -4,6 +4,27 @@ class Marker {
     this.color = color;
     this. remainingInk = remainingInk;
   }
+
+  write(word) {
+    let charsToWrite = 0;
+
+    if (this.remainingInk < word.length) {
+      let inkCount = this.remainingInk;
+        for (let i = 0; i < word.length; i++) {
+          if (word[i] !== ' ') {inkCount--;}
+          charsToWrite++;
+          if (inkCount <= 0) {break};
+        }
+    } else {
+      charsToWrite = word.length;
+    }
+
+    this.remainingInk -= word.split(' ').join('').length;
+    if (this.remainingInk <0) {this.remainingInk = 0};
+
+    return word.substring(0, charsToWrite);
+  }
+
 }
 
 module.exports = Marker
