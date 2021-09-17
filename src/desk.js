b115f774d62dfaa54b00a05ef2c0876d983a03a5
James Haggerty <jpm.haggerty@gmail.com>
2021-09-16 20:34:07 -0400

    Declare partial desk class

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,19 @@
 class Desk {
-  
+  constructor(length, width, height, isWhiteBoard) {
+    this.length = length;
+    this.width = width;
+    this. height = height;
+    this.isWhiteboard = isWhiteBoard;
+    this.content = '';
+  }
+
+  write(inputStr) {
+    this.content += inputStr;
+  }
+
+  wipe () {
+    if(isWhiteBoard) {this.content = ''}
+  }
 }
 
 module.exports = Desk
--- src/student.js
+++ src/student.js
@@ -1,5 +1,10 @@
 class Student {
-  
+  constructor(name, skillLevel) {
+    this.name = name;
+    this.skillLevel = skillLevel;
+    this.assignments = [];
+  }
+
 }
 
 module.exports = Student
